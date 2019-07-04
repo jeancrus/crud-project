85cb9414023f41450078dd60e0aeb8a240e9a22e
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-04 10:58:43 -0300

    Finishing the menu nav component

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 
 export default props => {
     return <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Refatorar em casa! */}
+            <a href="#/">
+                <i className="fa fa-home"></i>&ensp;Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i>&ensp;Usuários
+            </a>
+        </nav>
     </aside>
 }
