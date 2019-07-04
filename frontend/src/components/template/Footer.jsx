f5ded876445f4d05f2753b8a3da9716b3ed23b19
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-04 10:39:25 -0300

    Finishing Footer component

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 
 export default props => {
     return <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i>&ensp;
+            por
+            <strong>&ensp;Cod<span className="text-danger">3</span>r</strong>
+        </span>
     </footer>
 }
