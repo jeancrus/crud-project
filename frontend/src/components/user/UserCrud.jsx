be78e2b8c2e794d26421449431545cfee33b735a
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-04 11:27:58 -0300

    Init the component of UserCrud with state class

---
+++ frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Main from '../template/Main'
+
+const headerProps = {
+    icon: 'users',
+    title: 'Usuários',
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
+}
+
+// Separar os componentes tarefa
+
+export default class UserCrud extends Component {
+    render() {
+        return (
+            <Main {...headerProps}> 
+                Cadastro de Usuário
+            </Main>
+        )
+    }
+}
