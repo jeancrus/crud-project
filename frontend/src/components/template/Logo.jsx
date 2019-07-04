19330dc609fd8eae60a188fa17591697707482ca
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-04 09:50:28 -0300

    Introduction of Logo.css

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,8 +1,12 @@
 import './Logo.css'
+import logo from '../../assets/images/logo.png'
+
 import React from 'react'
 
 export default props => {
     return <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
 }
