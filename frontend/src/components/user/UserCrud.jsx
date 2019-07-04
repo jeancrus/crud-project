ecdc602192050b29956e649ab8e4861abadfb196
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-04 12:51:52 -0300

    Refactoring code block with error, axio to axios

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -27,7 +27,7 @@ export default class UserCrud extends Component {
         const user = this.state.user
         const method = user.id ? 'put' : 'post'
         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
-        axio[method](url, user)
+        axios[method](url, user)
             .then(resp => {
                 const list = this.getUpdatedList(resp.data)
                 this.setState({ user: initialState.user, list })
