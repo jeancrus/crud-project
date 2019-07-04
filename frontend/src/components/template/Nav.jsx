87558271d8e88c51bbf527551a4d2efe78419b0c
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-04 12:22:13 -0300

    Navegation routes for the SPA complete

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,16 +1,17 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => {
     return <aside className="menu-area">
         <nav className="menu">
-            {/* Refatorar em casa! */}
-            <a href="#/">
+            {/* Refatorar em casa! o link tera de ir pro componente refatorado*/}
+            <Link to="/">
                 <i className="fa fa-home"></i>&ensp;Início
-            </a>
-            <a href="#/users">
+            </Link>
+            <Link to="/users">
                 <i className="fa fa-users"></i>&ensp;Usuários
-            </a>
+            </Link>
         </nav>
     </aside>
 }
