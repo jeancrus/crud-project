f9edac46b3a530e0b26aeca625050f8e49124843
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-05 10:38:34 -0300

    Finish of the update and remove

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 export default props => {
     return <footer className="footer">
         <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i>&ensp;
-            por
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
             <strong>&ensp;Cod<span className="text-danger">3</span>r</strong>
         </span>
     </footer>
